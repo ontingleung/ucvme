22eec25a764416db825fcce84d9fbde9785e9cca
Onting <onting@Ontings-MacBook-Air.local>
2024-02-20 21:09:38 +0000

    firebase added

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqZK7lRUWl6IykFqmj6UlITHc4HeVACAM",
+  authDomain: "ucvme-global.firebaseapp.com",
+  projectId: "ucvme-global",
+  storageBucket: "ucvme-global.appspot.com",
+  messagingSenderId: "418189691386",
+  appId: "1:418189691386:web:d858edea36cccd6e093c43"
+};
+
+const app = initializeApp(firebaseConfig);
