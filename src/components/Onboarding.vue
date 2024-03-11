0ca86f8edfdd32a12b14e40a23082d39ece9f9b5
Onting <ontingleung@tutamail.com>
2024-03-11 15:48:09 +0900

    wizard-like form

--- src/components/Onboarding.vue
+++ src/components/Onboarding.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="max-w-screen-sm mx-auto shadow bg-white md:rounded-lg flex justify-center">
-        <div class="pb-12 m-10">
-            <component :is="currentStepComponent" />
-            <button v-if="currentStep !== 0" @click="previousStep" class="mt-4 bg-gray-500 text-white py-2 px-4 rounded-full">Previous</button>
-            <button @click='nextStep' class="mt-4 bg-indigo-500 text-white py-2 px-4 rounded-full">Next</button>
-        </div>
+  <div class="max-w-screen-sm mx-auto shadow bg-white md:rounded-lg">
+    <div class="pb-12 m-20">
+      <component
+        :is="currentStepComponent"
+        v-on:next="nextStep"
+        :firstName="firstName"
+        :lastName="lastName"
+        :town="town"
+        :county="county"
+        v-on:update="updateData"
+      />
     </div>
+  </div>
 </template>
+
 
 <script setup>
 import { ref, computed } from 'vue';
@@ -14,6 +21,11 @@ import Step1 from '@/components/steps/Step1.vue';
 import Step2 from '@/components/steps/Step2.vue';
 
 const currentStep = ref(0);
+
+const firstName = ref("")
+const lastName = ref("")
+const town = ref("")
+const county = ref("")
 
 const steps = [
   { number: 0, component: Step1 },
@@ -25,21 +37,17 @@ const currentStepComponent = computed(() => {
   return step ? step.component : null;
 });
 
-const nextStep = () => {
-console.log(currentStep.value)
-
-  if (currentStep.value < steps.length) {
-    currentStep.value++;
-  }
+const updateData = (data) => {
+  firstName.value = data.first
+  lastName.value = data.last
+  
 };
 
-const previousStep = () => {
-console.log(currentStep.value)
 
-  if (currentStep.value < steps.length) {
-    currentStep.value--;
-  }
+const nextStep = (step) => {
+  currentStep.value = step;
 };
 
 
 </script>
+
--- src/components/steps/Step2.vue
+++ src/components/steps/Step2.vue
@@ -1,58 +1,74 @@
 <script setup>
+import { defineProps, ref} from 'vue';
+
+const props = defineProps({
+  firstName: String,
+  lastName: String,
+  town: String,
+  county: String
+});
+
+
 
 </script>
 
 <template>
     <div>
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Step 2 of 4: Location</h2>
+        <h2 class="pt-5 text-base font-semibold leading-7 text-gray-900">Step 2 of 4: Location</h2>
         <div class="m-1 overflow-hidden rounded-full bg-gray-200">
-            <div class="h-2 w-2/4 rounded-full bg-emerald-400"></div>
+            <div class="h-2 w-1/2 rounded-full bg-emerald-400"></div>
         </div>
 
         <p class="mt-5 text-md leading-6 text-gray-600">See jobs listing close to you</p>
+        <div class="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+                <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Town</label>
+                <div class="mt-2">
+                    <input type="text" name="city" id="city" autocomplete="address-level2"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+            </div>
 
-        <div class="sm:col-span-2 sm:col-start-1">
-            <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Town</label>
-            <div class="mt-2">
-                <input type="text" name="city" id="city" autocomplete="address-level2"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <div class="sm:col-span-3">
+                <label for="county" class="block text-sm font-medium leading-6 text-gray-900">County</label>
+                <div class="mt-2">
+                    <select id="county" name="county" autocomplete="county-name"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                        <option disabled selected>Pick</option>
+                        <option>Carlow</option>
+                        <option>Cavan</option>
+                        <option>Clare</option>
+                        <option>Cork</option>
+                        <option>Donegal</option>
+                        <option>Dublin</option>
+                        <option>Galway</option>
+                        <option>Kerry</option>
+                        <option>Kildare</option>
+                        <option>Kilkenny</option>
+                        <option>Laois</option>
+                        <option>Leitrim</option>
+                        <option>Limerick</option>
+                        <option>Longford</option>
+                        <option>Louth</option>
+                        <option>Mayo</option>
+                        <option>Meath</option>
+                        <option>Monaghan</option>
+                        <option>Offaly</option>
+                        <option>Roscommon</option>
+                        <option>Sligo</option>
+                        <option>Tipperary</option>
+                        <option>Waterford</option>
+                        <option>Westmeath</option>
+                        <option>Wexford</option>
+                        <option>Wicklow</option>
+                    </select>
+                </div>
             </div>
         </div>
-
-        <div class="sm:col-span-3">
-            <label for="county" class="block text-sm font-medium leading-6 text-gray-900">County</label>
-            <div class="mt-2">
-                <select id="county" name="county" autocomplete="county-name"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option disabled selected>Pick</option>
-                    <option>Carlow</option>
-                    <option>Cavan</option>
-                    <option>Clare</option>
-                    <option>Cork</option>
-                    <option>Donegal</option>
-                    <option>Dublin</option>
-                    <option>Galway</option>
-                    <option>Kerry</option>
-                    <option>Kildare</option>
-                    <option>Kilkenny</option>
-                    <option>Laois</option>
-                    <option>Leitrim</option>
-                    <option>Limerick</option>
-                    <option>Longford</option>
-                    <option>Louth</option>
-                    <option>Mayo</option>
-                    <option>Meath</option>
-                    <option>Monaghan</option>
-                    <option>Offaly</option>
-                    <option>Roscommon</option>
-                    <option>Sligo</option>
-                    <option>Tipperary</option>
-                    <option>Waterford</option>
-                    <option>Westmeath</option>
-                    <option>Wexford</option>
-                    <option>Wicklow</option>
-                </select>
-            </div>
+        <div class="flex justify-between mt-10">
+            <button v-if="currentStep !== 0" @click="$emit('next', 0)"
+                class="bg-gray-500 text-white py-2 px-4 rounded-full">Previous</button>
+            <button @click="$emit('next', 2)" class="bg-indigo-500 text-white py-2 px-4 rounded-full">Next</button>
         </div>
     </div>
 </template>
