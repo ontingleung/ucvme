724b9160e2cbbbaa60c5a1a28391b42498e8fb51
carolinesh <carolinesh@uia.no>
2024-03-14 10:58:17 +0000

    Added a homepage draft

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4" > Welcome to UVCME! </h1>
+
+        <p class="text-lg text-gray-600" > 
+            This is a platform for you to get easisly get in contact with your locals! 
+            View their CV videoes and get the job done!! 
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+/* Add custom styles here if needed */
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/homePage',
+      name: 'homePage',
+      component: () => import('../views/HomePageView.vue')
+    },
+    {
       path: '/signup',
       name: 'signup',
       component: () => import('../views/SignUpView.vue')
