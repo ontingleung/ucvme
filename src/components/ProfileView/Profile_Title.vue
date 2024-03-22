518a5004ce9a522be6716623af2963825bb6a7a7
marc-rene <95175463+marc-rene@users.noreply.github.com>
2024-03-22 02:16:22 +0100

    Created Profile view
    Profile view can ONLY be accessed when route is provided with a valid user ID

---
+++ src/components/ProfileView/Profile_Title.vue
@@ -0,0 +1,38 @@
+<!-- 
+
+This is the title for each Profile
+
+"John, 27 years old from Cork"
+
+-->
+
+<script setup lang="ts">
+
+import { defineProps, withDefaults } from 'vue';
+
+export interface Props {
+    profile_title: string,
+    profile_town: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    profile_title: 'Loading...'
+});
+
+</script>
+
+<template>
+
+    <div class="flex justify-center items-center">
+        <h1 class="text-2xl font-bold text-center p-4 bg-white/60 backdrop-blur-md shadow-md">
+            {{ props.profile_title }}, from {{ profile_town }}
+        </h1>
+    </div>
+
+</template>
+
+
+
+<style>
+
+</style>
---
+++ src/components/ProfileView/Profile_Video.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+import { VideoPlayer } from '@videojs-player/vue';
+import 'video.js/dist/video-js.css';
+
+
+const props = withDefaults(defineProps<{
+    video_url: string,
+    poster_url: string;
+}>(), {
+    video_url: 'NO URL PROVIDED',
+    poster_url: "/TEST_Photo_Delete_later.gif"
+});
+
+</script>
+
+
+
+
+<template>
+    <div class="flex justify-center items-center  ">
+        <div class="video-container rounded-lg overflow-hidden " style="height: 50vh; width: 50%;">
+            <video-player 
+                :src="props.video_url" 
+                class="ucvme-theme" 
+                :poster="props.poster_url" 
+                controls
+                :enableDocumentPictureInPicture="false" 
+                :loop="true" 
+                :autoplay="true" 
+                :volume="0.6" />
+        </div>
+    </div>
+    
+</template>
+
+
+
+<style>
+@import url(/src/assets/VideoJS/video-js.min.css);
+@import url(/src/assets/VideoJS/UCVME_theme.css);
+
+.ucvme-theme {
+height: 100%;
+width: 100%;
+   /* This makes the video cover the area of the box, adjust as needed */
+}
+</style>
